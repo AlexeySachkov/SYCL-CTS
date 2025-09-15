7055f60149575f7bee9c03595874c6d187d7978a
Alexey Sachkov <alexey.sachkov@intel.com>
2025-09-15 14:45:43 +0200

    Add placeholders for other headers

---
+++ tests/extension/khr_includes/byte.cpp
@@ -0,0 +1,37 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Copyright (c) 2025 The Khronos Group Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+*******************************************************************************/
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <sycl/khr/includes/byte.hpp>
+
+namespace khr_includes::tests {
+
+TEST_CASE("the implementation defines the SYCL_KHR_INCLUDES macro",
+          "[khr_includes][device]") {
+#ifdef SYCL_KHR_INCLUDES
+  constexpr bool macroIsDefined = true;
+#else
+  constexpr bool macroIsDefined = false;
+#endif
+  STATIC_REQUIRE(macroIsDefined);
+}
+
+}  // namespace khr_includes::tests
